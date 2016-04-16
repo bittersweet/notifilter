bedd4c2336639db96aa6c1e6cd986ab3f0f67d03
Mark Mulder <markmulder@gmail.com>
2016-04-16 23:19:32 +0200

    Show total amount of notifiers

--- web/controllers/notifier_controller.ex
+++ web/controllers/notifier_controller.ex
@@ -1,11 +1,14 @@
 defmodule Notifilter.NotifierController do
   use Notifilter.Web, :controller
+  import Ecto.Query
 
   alias Notifilter.Notifier
 
   def index(conn, _params) do
     notifiers = Repo.all(Notifier)
-    render conn, "index.html", notifiers: notifiers
+    query = from n in Notifier, select: count(n.id)
+    count = Repo.one(query)
+    render conn, "index.html", count: count, notifiers: notifiers
   end
 
   def show(conn, %{"id" => id}) do
