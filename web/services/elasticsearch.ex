74e0f6bfe05c310f2c81c328bb1c9ce0a1c9d160
Mark Mulder <markmulder@gmail.com>
2016-04-19 17:03:41 +0200

    Implement fetching of all applications

--- web/services/elasticsearch.ex
+++ web/services/elasticsearch.ex
@@ -12,12 +12,41 @@ defmodule Notifilter.Elasticsearch do
   end
 
   def event(event) do
-    url = "localhost:9200/notifilter/event/#{event}?pretty=true"
+    url = "localhost:9200/notifilter/event/#{event}"
     {:ok, response} = HTTPoison.get(url, [])
     Poison.decode!(response.body)["_source"]
   end
 
+  @doc """
+  Query ES for all known applications we have seen so far.
+  """
+  def applications do
+    url = "localhost:9200/notifilter/event/_search"
+    headers = []
+    query = %{
+      "size": 0,
+      "aggs": %{
+        "applications": %{
+          "terms": %{
+            "field": "application",
+            "size": 0, # Make sure we get all results, not limited
+            "order": %{
+              "_term": "asc"
+            }
+          }
+        }
+      }
+    }
+    body = Poison.encode!(query)
+
+    {:ok, response} = HTTPoison.post(url, body, headers)
+    result = Poison.decode!(response.body)
+    keys = result["aggregations"]["applications"]["buckets"]
+    Enum.map(keys, fn(bucket) -> bucket["key"] end)
+  end
+
   def handle_response({ :error, response }) do
+    IO.inspect response
   end
 
   def handle_response({ :ok, response }) do
