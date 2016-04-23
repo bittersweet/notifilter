3a6fa98b7e8f294a275c4335e5735d3fc3c0c24d
Mark Mulder <markmulder@gmail.com>
2016-04-23 21:55:12 +0200

    Set test credentials

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :notifilter, Notifilter.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "markmulder",
+  password: "markmulder",
   database: "notifilter_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
