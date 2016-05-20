0e9992d86314c8c3846e25866b6a7c8b84c4ccde
Mark Mulder <markmulder@gmail.com>
2016-05-20 14:13:15 +0200

    Add exrm

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule Notifilter.Mixfile do
      {:cowboy, "~> 1.0"},
      {:httpoison, "~> 0.8.2"},
      {:oauth2, "~> 0.5"},
+     {:exrm, "~> 1.0.4"},
     {:credo, "~> 0.3", only: [:dev, :test]}]
   end
 
