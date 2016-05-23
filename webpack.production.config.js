84209f71836e008615c454c0d48348f3a3144214
Mark Mulder <markmulder@gmail.com>
2016-05-23 15:40:09 +0200

    Add production webpack config
    This will use the production build of React+Redux, and remove dead code.
    
    `npm run deploy`

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,10 +1,21 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: "./web/static/js/index.js",
     output: {
         path: "./priv/static/js",
         filename: "app.js"
     },
-
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+    ],
     module: {
         loaders: [
             {
