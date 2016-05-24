196c19e1c1084bd1dc6eb2661ed523c028e0855d
Mark Mulder <markmulder@gmail.com>
2016-05-24 15:42:34 +0200

    Remove outdated TODO

--- web/static/js/components/preview_button.js
+++ web/static/js/components/preview_button.js
@@ -6,12 +6,7 @@ var PreviewButton = React.createClass({
         event.preventDefault();
 
         const { actions } = this.props;
-
-        // TODO: refactor
-        // needs to grab data from local server and add that
-        // For now it's stubbed in actions.js
         const { application, template, eventName } = Store.getState();
-        console.log('application:', application, 'template:', template, 'eventName:', eventName);
 
         actions.getPreview(application, eventName, template);
     },
