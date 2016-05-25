e55bb6fe51d95cba04ded30a21fea81bb408460a
Mark Mulder <markmulder@gmail.com>
2016-05-25 21:38:34 +0200

    Setup webpack to load scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-    entry: "./web/static/js/index.js",
-    output: {
-        path: "./priv/static/js",
-        filename: "app.js"
-    },
+  entry: "./web/static/js/index.js",
+  output: {
+    path: "./priv/static/js",
+    filename: "app.js"
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,31 +1,35 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: "./web/static/js/index.js",
-    output: {
-        path: "./priv/static/js",
-        filename: "app.js"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"'
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
+  entry: "./web/static/js/index.js",
+  output: {
+    path: "./priv/static/js",
+    filename: "app.js"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
 };
