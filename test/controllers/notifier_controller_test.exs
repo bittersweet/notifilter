35923ff6d427094edd82d88e87e9ccd03fd063ce
Mark Mulder <markmulder@gmail.com>
2016-06-01 11:34:00 +0200

    Test notifier controller

---
+++ test/controllers/notifier_controller_test.exs
@@ -0,0 +1,32 @@
+defmodule Notifilter.NotifierControllerTest do
+  use Notifilter.ConnCase
+
+  def insert_notifier(attrs \\ %{}) do
+    changes = Dict.merge(%{
+      application: "Application 1",
+      event_name: "conversion",
+      template: "New conversion by user",
+      rules: [%{}],
+      notification_type: "slack",
+      target: "#test",
+    }, attrs)
+
+    %Notifilter.Notifier{}
+    |> Notifilter.Notifier.changeset(changes)
+    |> Repo.insert!
+  end
+
+  test "redirects to auth" do
+    conn = get conn, "/"
+    assert html_response(conn, 302)
+  end
+
+  test "shows notifiers when logged in" do
+    insert_notifier(%{application: "Notifilter Testapp"})
+
+    user = %{name: "mark", avatar: "", email: "mark@springest.com"}
+    conn = assign(conn(), :current_user, user)
+    conn = get conn, notifier_path(conn, :index)
+    assert html_response(conn, 200) =~ "Notifilter Testapp"
+  end
+end
