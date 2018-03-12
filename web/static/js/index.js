f85e6cda480efffd2002385e132a160175114ecb
Mark Mulder <markmulder@gmail.com>
2018-03-12 11:37:33 +0100

    Correct capitalization on app.js file

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './app';
 import store from './store';
 
 let rootElement = document.getElementById('content');
