7707568dfbe3c68aa13233b2856f5b29e519e5ea
Mark Mulder <markmulder@gmail.com>
2018-03-14 12:52:04 +0100

    Require SSL in production
    This requires the NOTIFILTER_HOSTNAME environment variable to be set.

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :notifilter, Notifilter.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: System.get_env("NOTIFILTER_HOSTNAME"), port: 443],
+  force_ssl: [hsts: true],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
